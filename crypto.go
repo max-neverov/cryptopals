7f867f465f1fc1ce3c435cf694aee5a8fb7b73ba
Maxim Neverov <neverov.max@gmail.com>
2019-03-16 16:58:47 +0100

    add set 1 challenge 3 solution: decipher by single xor and char frequencies

--- crypto.go
+++ crypto.go
@@ -5,9 +5,44 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"sync"
+	"unicode"
 )
 
 const base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+var freqs = map[int32]float64{
+	' ':  0.04200,
+	'.':  0.02100,
+	',':  0.00110,
+	'\'': 0.00070,
+	'a':  0.08167,
+	'b':  0.01492,
+	'c':  0.02782,
+	'd':  0.04253,
+	'e':  0.12702,
+	'f':  0.02228,
+	'g':  0.02015,
+	'h':  0.06094,
+	'i':  0.06966,
+	'j':  0.00153,
+	'k':  0.00772,
+	'l':  0.04025,
+	'm':  0.02406,
+	'n':  0.06749,
+	'o':  0.07507,
+	'p':  0.01929,
+	'q':  0.00095,
+	'r':  0.05987,
+	's':  0.06327,
+	't':  0.09056,
+	'u':  0.02758,
+	'v':  0.00978,
+	'w':  0.02360,
+	'x':  0.00150,
+	'y':  0.01974,
+	'z':  0.00074,
+}
 
 // ToBase64 encodes bytes read by given reader and returns base64 encoded string.
 //
@@ -81,6 +116,60 @@ func FixedXor(l, r io.Reader) ([]byte, error) {
 	dst := make([]byte, hex.EncodedLen(len(src)))
 	hex.Encode(dst, src)
 	return dst, nil
+}
+
+// SingleByteXorDecipher decodes given byte stream by using xor with a single
+// char from `base64Table` and calculating max sentence rate by char frequencies
+func SingleByteXorDecipher(r io.Reader) (string, error) {
+	bs, err := decodeHexToBytes(r)
+	if err != nil {
+		return "", err
+	}
+
+	type decodeResult struct {
+		fr float64
+		bs []byte
+	}
+	ch := make(chan decodeResult)
+	var wg sync.WaitGroup
+
+	// decode mutates input slice of bytes
+	decode := func(in []byte, b byte) {
+		defer wg.Done()
+
+		var fr = 0.0
+		var nonASCIICharPenalty = 0.05
+		for i := range in {
+			in[i] ^= b
+			if v, ok := freqs[unicode.ToLower(int32(in[i]))]; !ok {
+				fr -= nonASCIICharPenalty
+			} else {
+				fr += v
+			}
+		}
+
+		ch <- decodeResult{fr, in}
+	}
+
+	for i := range base64Table {
+		wg.Add(1)
+		go decode(append(bs[:0:0], bs...), base64Table[i])
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	rate := -1.0
+	var resBytes []byte
+	for r := range ch {
+		if rate < r.fr {
+			rate = r.fr
+			resBytes = r.bs
+		}
+	}
+	return string(resBytes), nil
 }
 
 func decodeHexToBytes(r io.Reader) ([]byte, error) {
--- crypto_test.go
+++ crypto_test.go
@@ -40,3 +40,14 @@ func TestFixedXor(t *testing.T) {
 		t.Errorf("Expected %q, got %q", expected, actual)
 	}
 }
+
+func TestSingleByteXorDecipher(t *testing.T) {
+	actual, err := SingleByteXorDecipher(strings.NewReader("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
+	if err != nil {
+		t.Errorf("Error while decipher %v", err)
+	}
+	expected := "Cooking MC's like a pound of bacon"
+	if actual != expected {
+		t.Errorf("Expected %q got %q", expected, actual)
+	}
+}
